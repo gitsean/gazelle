10e17153df3daf649e0775f12e3853edd37b8d3e
Sean <sareid@outlook.com>
2020-09-02 06:05:14 -0400

    Uploader component

--- client/src/components/Uploader.spec.js
+++ client/src/components/Uploader.spec.js
@@ -2,8 +2,19 @@ import { shallowMount } from "@vue/test-utils";
 import Uploader from "@/components/Uploader.vue";
 
 describe("Uploader.vue", () => {
-  it("renders upload button when loaded", () => {
-    const wrapper = shallowMount(Uploader);
-    expect(wrapper.contains("form")).toBe(true);
+  it("initial status renders form when loaded", () => {
+    const wrapper = shallowMount(Uploader, {
+      data() {
+        return {
+          uploadedFiles: [],
+          uploadError: null,
+          currentStatus: 0,
+          uploadFieldName: "csv",
+        };
+      },
+    });
+    const div = wrapper.find("div");
+    const form = div.find("form");
+    expect(form.exists()).toBe(true);
   });
 });
