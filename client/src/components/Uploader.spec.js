3effbb1bb4eebbd21525d867ea55707afac38ba0
Sean <sareid@outlook.com>
2020-09-02 06:39:43 -0400

    Input accept one file

--- client/src/components/Uploader.spec.js
+++ client/src/components/Uploader.spec.js
@@ -1,20 +1,32 @@
 import { shallowMount } from "@vue/test-utils";
 import Uploader from "@/components/Uploader.vue";
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallowMount(Uploader, {
+    data() {
+      return {
+        uploadedFiles: [],
+        uploadError: null,
+        currentStatus: 0,
+        uploadFieldName: "csv",
+      };
+    },
+  });
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
 describe("Uploader.vue", () => {
   it("initial status renders form when loaded", () => {
-    const wrapper = shallowMount(Uploader, {
-      data() {
-        return {
-          uploadedFiles: [],
-          uploadError: null,
-          currentStatus: 0,
-          uploadFieldName: "csv",
-        };
-      },
-    });
-    const div = wrapper.find("div");
-    const form = div.find("form");
+    const form = wrapper.find("form");
     expect(form.exists()).toBe(true);
+  });
+  it("form allows only one upload", () => {
+    const input = wrapper.find("input");
+    expect(input.attributes().multiple).toBe(undefined);
   });
 });
