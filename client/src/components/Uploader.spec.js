8bee5ee33e28220ec28ced42b4a259d5be8d7e70
Sean <sareid@outlook.com>
2020-09-05 06:28:45 -0400

    Test only accept csv

--- client/src/components/Uploader.spec.js
+++ client/src/components/Uploader.spec.js
@@ -25,27 +25,33 @@ describe("Uploader.vue", () => {
     const form = wrapper.find("form");
     expect(form.exists()).toBe(true);
   });
+
   it("form allows only one upload", () => {
     const input = wrapper.find("input");
     expect(input.attributes().multiple).toBe(undefined);
   });
+
+  it("input allows only CSV file type", () => {
+    const input = wrapper.find("input");
+    expect(input.attributes().accept).toBe("csv");
+  });
+
   it("shows success message upon successful upload", async () => {
     await wrapper.setData({
       uploadedFiles: ["file.csv"],
       uploadError: false,
       currentStatus: 2,
     });
-
     const success = wrapper.find("h2");
     expect(success.text()).toMatch("Uploaded file successfully.");
   });
+
   it("shows failed message upon failed upload", async () => {
     await wrapper.setData({
       uploadedFiles: null,
       uploadError: true,
       currentStatus: 3,
     });
-
     const success = wrapper.find("h2");
     expect(success.text()).toMatch("Upload failed.");
   });
