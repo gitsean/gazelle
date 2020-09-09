6756f802f93ada04bb391bc964f407ef0ae5e80b
Sean <sareid@outlook.com>
2020-09-08 23:55:59 -0400

    Format about just a little

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="about">
+  <div class="about container">
     <h1>What is this?</h1>
     <p>This is a sample project to showcase the capabilities of Vue with Flask API. You should be uploading CSV files with the following headers and corresponding data.</p>
     <p>
-    <table>
+    <table class="table">
       <th>
         <td>guid</td>
         <td>name</td>
